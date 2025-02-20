e2024b1bb9f589a17acb8a4e10e4bb194fb24a1b
LUCCA <lsanderf08@gmail.com>
2025-02-20 18:30:59 -0300

    Código de soma pronto

--- src/ti2/Soma.java
+++ src/ti2/Soma.java
@@ -1,10 +1,22 @@
 package ti2;
 
+import java.util.Scanner;
+
 public class Soma {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			Scanner scanner = new Scanner(System.in);
+		
+			System.out.println("Digite o 1.o número: \n");
+			int num1 = scanner.nextInt();
+			
+			System.out.println("Digite o 2.o número: \n");
+			int num2 = scanner.nextInt();
+			
+			int resultado = num1 + num2;
+			System.out.println("A soma é: "+ resultado);
+			scanner.close();
+		}
 
-	}
 
 }
